e4278ed9d481e01846f4deba11a3ce072e4d7630
AlCode88 <alanburkanov@gmail.com>
2021-03-03 13:36:17 -0600

    updates

---
+++ Practice_challanges/Dynamic_Blocks/Dynamic-Example.tf
@@ -0,0 +1,46 @@
+resource "aws_instance" "terraformEC2" {
+ instance_type = "t2.micro"
+ ami = "ami-0915bcb5fa77e4892"
+ security_groups = [aws_security_group.terraformSG.name ]
+ tags = {
+   "Name" = "TerraformPractice"
+ }
+}
+variable "ingress"{
+  type = list(number)
+  default = [80,443]
+}                                       
+variable "egress"{
+  type = list(number)
+  default = [80,443,25,3306]
+}                                               
+resource "aws_security_group" "terraformSG" {
+  name        = "terraformEC2"
+  description = "allow the trafic from 22"
+  
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress
+    content {
+     from_port = port.value
+     to_port = port.value
+     protocol = "TCP"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    content {
+     from_port = port.value
+     to_port = port.value
+     protocol = "TCP"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+tags = {
+    Name = "terraformEC2"
+  }
+}                                                
