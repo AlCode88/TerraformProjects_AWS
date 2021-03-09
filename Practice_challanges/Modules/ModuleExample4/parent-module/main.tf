b7abc8d3d6c96a580f02c1d272bae76a435ec93a
AlCode88 <alanburkanov@gmail.com>
2021-03-08 23:24:30 -0600

    module validatationsuccesful

--- Practice_challanges/Modules/ModuleExample4/parent-module/main.tf
+++ Practice_challanges/Modules/ModuleExample4/parent-module/main.tf
@@ -3,8 +3,17 @@
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
 }
-# modules
+# you need to always run terraform init whenever you make a changes
+# on your module. 
+#Need to be in the parent directory or file to run terraform init
+
 module "any-name-module" {
-    source = "../modules/child-module" #source is the only one that is required
-    
+    #source is the only one that is required
+    source = "../modules/child-module" 
+    #all the followings are coming from the modules/child-module/variables.tf
+    vpc = aws_vpc.main.id        
+    cidr_block = "10.0.0.0/16"
+    ami = "ami-047a51fa27710816e"
+    instance_type = "t2.medium"
+    webserver_name = "test"
 }
