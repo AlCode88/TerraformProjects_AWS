8eb4afeb3c27ccfa1407e9408f79367f51264808
AlCode88 <alanburkanov@gmail.com>
2021-03-08 21:32:47 -0600

    update

--- ModulesPractice/modules/webserver/ouputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance" {
-  value = aws_instance.webserver_module
-  description = "webserver_content"
-}
--- Practice_challanges/Modules/ModuleExample3/eip/eip.tf
+++ Practice_challanges/Modules/ModuleExample3/eip/eip.tf
@@ -1,7 +1,7 @@
 # 8  Assign An Elastic IP to the Network Interface Created in step 7 
 resource "aws_eip" "one" {
   vpc                       = true
-  network_interface         = var.network_interface
+  network_interface         = aws_network_interface.terraform_network_interface.id
   associate_with_private_ip = "10.0.1.50"
   depends_on                = [aws_internet_gateway.terraform_project1_Internet_Gateway]
 }
@@ -10,7 +10,3 @@ output "aws-eip" {
   value = aws_eip.one.id
 }
 
-variable "network_interface" {
-  type = string
-  defaule = aws_network_interface.terraform_network_interface.id
-}
