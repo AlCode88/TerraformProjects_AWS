4952102fea94d1b72d7b3e0436d877c012a946a4
EC2 Default User <ec2-user@ip-172-31-82-145.ec2.internal>
2021-04-09 00:29:31 +0000

    task udates

--- Terraform_Session_April/session2/session2_task/main.tf
+++ Terraform_Session_April/session2/session2_task/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
  region = "us-west-1"
 }
+
+resource "aws_instance" "Webserver" {
+  instance_type = "t2.micro"
+  ami           = "ami-0915bcb5fa77e4892"
+  user_data = file("userdata.sh")
+  security_groups = [ aws_security_group.taskSG.name ]
+  tags = {
+    "Name" = "Web_instance"
+  }
+}
+
+
---
+++ Terraform_Session_April/session2/session2_task/security_group.tf
@@ -0,0 +1,37 @@
+# This is dynamic SG variables
+variable "ingress" {
+type = list (number)
+default = [80,22]
+}
+
+variable "egress" {
+    type = list (number)
+    default = [80,22]
+}
+
+
+resource "aws_security_group" "taskSG" {
+  name        = "task_session2_SG"
+  description = "This webserver SG"
+  
+dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress
+    content {
+     from_port = port.value
+     to_port = port.value
+     protocol = "TCP"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    content {
+     from_port = port.value
+     to_port = port.value
+     protocol = "TCP"
+     cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
