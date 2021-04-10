c1850776093afc803e1d4501fbd9bc9ad8de835b
EC2 Default User <ec2-user@ip-172-31-82-145.ec2.internal>
2021-04-10 23:47:12 +0000

    updates

--- Terraform_Session_April/session3/EC2.tf
+++ Terraform_Session_April/session3/EC2.tf
@@ -1,12 +1,9 @@
 resource "aws_instance" "session3_EC2" {
-  ami           = "ami-0742b4e673072066f"
-  instance_type = "t2.micro"
-  user_data = <<EOF
-  #!/bin/bash
-  sudo yum install httpd -y
-  sudo echo "Hello from EC2" >> /var/www/html/index.html
-  sudo systemctl start httpd
-  EOF
+  ami                     = "ami-0742b4e673072066f"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.session3_SG.id]
+  key_name                = "My_Mac_keys"
+  user_data               = file("userdata.sh")
 
   tags = {
     Name = "Instance-1"
